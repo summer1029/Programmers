19700d85a947554e021b83062af62a82e2888017
summer1029 <148053142+summer1029@users.noreply.github.com>
2023-11-14 01:24:10 +0900

    [level unrated] Title: 문자열안에 문자열, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120908. 문자열안에 문자열/문자열안에 문자열.py
+++ 프로그래머스/unrated/120908. 문자열안에 문자열/문자열안에 문자열.py
@@ -1,8 +1,9 @@
 def solution(str1, str2):
     if len(str1) >= 1 and len(str1) <= 1000 and len(str2) >= 1 and len(str2) <= 1000 :
-        if str1.find(str2) >= 0 :
-            answer = 1
+        # .find는 존재하면 존재하는 문자열의 위치를 반환, 없으면 -1을 반환
+        if str1.find(str2) == -1 :
+            answer = 2
             return answer
         else : 
-            answer = 2
+            answer = 1
             return answer
