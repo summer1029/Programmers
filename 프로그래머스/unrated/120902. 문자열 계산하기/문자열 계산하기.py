805a85f59ae7d4e000295ef0c0e5d3c8172bdb81
summer1029 <148053142+summer1029@users.noreply.github.com>
2023-11-21 01:10:15 +0900

    [level unrated] Title: 문자열 계산하기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120902. 문자열 계산하기/문자열 계산하기.py
@@ -0,0 +1,17 @@
+def solution(my_string):
+    if len(my_string) >= 5 and len(my_string) <= 100 :
+        op = []
+        for i in range(len(my_string)) :
+            if my_string[i] == '+' or my_string[i] == '-' :
+                op.append(i)
+        sum = int(my_string[0 : op[0]])
+        for i in range(len(op) - 1) :
+            if my_string[op[i]] == "+" :
+                sum = sum + int(my_string[op[i] + 1 : op[i + 1]])
+            elif my_string[op[i]] == "-" :
+                sum = sum - int(my_string[op[i] + 1 : op[i + 1]])
+        if my_string[op[len(op)-1]] == "+" :
+            sum = sum + int(my_string[op[-1] + 1 : len(my_string)])
+        elif my_string[op[len(op)-1]] == "-" :
+            sum = sum - int(my_string[op[-1] + 1 : len(my_string)])
+    return sum
