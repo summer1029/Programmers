1e48aab4eea3bf5ba77b388d08ade4a8fab430c2
summer1029 <148053142+summer1029@users.noreply.github.com>
2023-11-24 00:16:22 +0900

    [level unrated] Title: 양꼬치, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120830. 양꼬치/양꼬치.py
+++ 프로그래머스/unrated/120830. 양꼬치/양꼬치.py
@@ -1,8 +1,8 @@
 def solution(n, k):
     price = 0
-    
-    price = n * 12000 + k * 2000
-    if n >= 10 :
-        price = price - 2000 * (n // 10)
+    if n > 0 and n < 1000 and k >= n // 10 and k < 1000 :
+        price = n * 12000 + k * 2000
+        if n >= 10 :
+            price = price - 2000 * (n // 10)
     
     return price
