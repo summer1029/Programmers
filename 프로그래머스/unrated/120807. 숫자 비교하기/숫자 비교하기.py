246967b8a9dec7217fed2d921739eedc9f919dd5
summer1029 <148053142+summer1029@users.noreply.github.com>
2023-11-23 23:28:11 +0900

    [level unrated] Title: 숫자 비교하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120807. 숫자 비교하기/숫자 비교하기.py
@@ -0,0 +1,7 @@
+def solution(num1, num2):
+    if num1 >= 0 and num1 <= 10000 and num2 >= 0 and num2 <= 10000 :
+        if (num1 == num2) :
+            answer = 1
+        else :
+            answer = -1
+    return answer
