3cf2305cf64be7be3cc8c2eeb1bcb00c5bb5f05c
summer1029 <148053142+summer1029@users.noreply.github.com>
2023-11-24 13:24:40 +0900

    [level unrated] Title: 최댓값 만들기 (1), Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.py
+++ 프로그래머스/unrated/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.py
@@ -1,15 +1,22 @@
 def solution(numbers):
-    first = 0
-    second = 0
+    # 방법 1
+#     first = 0
+#     second = 0
     
-    if (len(numbers) >= 2 and len(numbers) <= 100) :
-        for i in range(len(numbers)) :
-            if (numbers[i] >= 0 and numbers[i] <= 10000) :
-                if (numbers[i] > first) : # 원소가 first보다 더 크면
-                    second = first # second에 first원소를 넣고
-                    first = numbers[i] # first에 이보다 더 큰 원소를 넣는다
-                elif (second < numbers[i] <= first) : # 원소가 first보단 작으나 second보다 크면 
-                    second = numbers[i] # second만 갱신한다
+#     if (len(numbers) >= 2 and len(numbers) <= 100) :
+#         for i in range(len(numbers)) :
+#             if (numbers[i] >= 0 and numbers[i] <= 10000) :
+#                 if (numbers[i] > first) : # 원소가 first보다 더 크면
+#                     second = first # second에 first원소를 넣고
+#                     first = numbers[i] # first에 이보다 더 큰 원소를 넣는다
+#                 elif (second < numbers[i] <= first) : # 원소가 first보단 작으나 second보다 크면 
+#                     second = numbers[i] # second만 갱신한다
                         
-        answer = first * second
+#         answer = first * second
+
+    # 방법 2
+    numbers.sort()
+    first = numbers[-1]
+    second = numbers[-2]
+    answer = first * second
     return answer
