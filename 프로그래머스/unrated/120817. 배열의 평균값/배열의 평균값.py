7ada51edca828f7c5b9d648aef486af7a954ba05
summer1029 <148053142+summer1029@users.noreply.github.com>
2023-11-24 12:29:32 +0900

    [level unrated] Title: 배열의 평균값, Time: 0.01 ms, Memory: 10.1 MB -Baekj… …oonHub

--- 프로그래머스/unrated/120817. 배열의 평균값/배열의 평균값.py
+++ 프로그래머스/unrated/120817. 배열의 평균값/배열의 평균값.py
@@ -1,8 +1,20 @@
+# 방법1
+# def solution(numbers):
+#     sum = 0
+#     if(len(numbers) >= 1 and len(numbers) <= 100) :
+#         for i in range(len(numbers)) :
+#             if (numbers[i] >= 0 and numbers[i] <= 1000) :
+#                 sum = sum + numbers[i]
+#         answer = sum / len(numbers)
+#     return answer
+
+# 방법2
+import statistics
+
 def solution(numbers):
-    sum = 0
+    
     if(len(numbers) >= 1 and len(numbers) <= 100) :
         for i in range(len(numbers)) :
             if (numbers[i] >= 0 and numbers[i] <= 1000) :
-                sum = sum + numbers[i]
-        answer = sum / len(numbers)
+                answer = statistics.mean(numbers)
     return answer
