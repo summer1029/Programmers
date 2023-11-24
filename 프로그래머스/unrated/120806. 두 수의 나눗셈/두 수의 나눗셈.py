2af968921b34b8d0ae34a1e7dce3361a049689af
summer1029 <148053142+summer1029@users.noreply.github.com>
2023-11-24 21:38:37 +0900

    [level unrated] Title: 두 수의 나눗셈, Time: 0.01 ms, Memory: 10.1 MB -Bae… …k… …joonHub

--- 프로그래머스/unrated/120806. 두 수의 나눗셈/두 수의 나눗셈.py
+++ 프로그래머스/unrated/120806. 두 수의 나눗셈/두 수의 나눗셈.py
@@ -1,6 +1,6 @@
 import math
 
 def solution(num1, num2):
-if (num1 > 0 and num1 <= 100 and num2 > 0 and num2 <= 100) :
-answer = math.trunc(num1 / num2 * 1000)
-return answer
+    if (num1 > 0 and num1 <= 100 and num2 > 0 and num2 <= 100) :
+        answer = math.trunc(num1 / num2 * 1000)
+    return answer
