34bf659e417e5949712428855ab940222aa69678
summer1029 <148053142+summer1029@users.noreply.github.com>
2023-11-29 23:27:55 +0900

    [level unrated] Title: 한 번만 등장한 문자, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
+++ 프로그래머스/unrated/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
@@ -1,6 +1,19 @@
 def solution(s):
-    answer = []
-    for char in s :
-        if s.count(char) == 1 :
-            answer.append(char)
-    return "".join(sorted(answer))
+    # 방법 1
+    # answer = []
+    # for char in s :
+    #     if s.count(char) == 1 :
+    #         answer.append(char)
+    # return "".join(sorted(answer))
+    
+    # 방법 2
+    sort = sorted(s)
+    result = ""
+    for i in range(len(sort)) :
+        count = 0
+        for j in range(len(sort)) :
+            if sort[i] == sort[j] :
+                count = count + 1
+        if count == 1 :
+            result = result + sort[i]
+    return result
