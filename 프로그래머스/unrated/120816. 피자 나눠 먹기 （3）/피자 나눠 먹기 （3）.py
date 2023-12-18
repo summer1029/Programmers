83fc58a857e46bab791cce11ed9b0b82d2beed7a
summer1029 <148053142+summer1029@users.noreply.github.com>
2023-12-18 12:30:31 +0900

    [level unrated] Title: 피자 나눠 먹기 (3), Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
+++ 프로그래머스/unrated/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
@@ -1,8 +1,18 @@
 def solution(slice, n):
-    cnt = 1
+    # 방법1
+    # cnt = 1
+    # if (slice >= 2 and slice <= 10 and n >= 1 and n <= 100) :
+    #     while n >= slice * cnt :
+    #         cnt = cnt + 1
+    #     if (n == slice * (cnt - 1)) :
+    #         cnt = cnt - 1 
+    #     return cnt
+    
+    # 방법2
     if (slice >= 2 and slice <= 10 and n >= 1 and n <= 100) :
-        while n >= slice * cnt :
-            cnt = cnt + 1
-        if (n == slice * (cnt - 1)) :
-            cnt = cnt - 1 
+        if (n % slice > 0) :
+            cnt = n // slice + 1
+        else :
+            cnt = n // slice
         return cnt
+    
