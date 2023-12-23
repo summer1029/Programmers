8ada1015e811f12114180474e7bb40f9baf74643
summer1029 <148053142+summer1029@users.noreply.github.com>
2023-12-24 01:44:18 +0900

    [level unrated] Title: 팩토리얼, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120848. 팩토리얼/팩토리얼.py
+++ 프로그래머스/unrated/120848. 팩토리얼/팩토리얼.py
@@ -7,11 +7,21 @@ def solution(n):
     #             result = i
     # return result
 
-    # 방법 1
-    if n > 0 and n <= 3628800 :
-        factorial = 1
-        for i in range(1, 11) :
-            factorial = factorial * i
-            if factorial <= n :
-                result = i
-        return result
+    # 방법 2
+    # if n > 0 and n <= 3628800 :
+    #     factorial = 1
+    #     for i in range(1, 11) :
+    #         factorial = factorial * i
+    #         if factorial <= n :
+    #             result = i
+    #     return result
+    
+    # 방법 3
+    factorial = 1
+    i = 1
+    while factorial <= n :
+        factorial = factorial * i
+        i = i + 1
+        if factorial > n :
+            i = i - 2
+    return i
